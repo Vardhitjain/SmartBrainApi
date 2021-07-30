553163375be80e296dd6946467ddf9c2739e954b
Vardhitjain <vardhit.jain11@gmail.com>
2021-07-30 13:49:28 +0530

    Server side of SmartBrain

---
+++ controllers/profile.js
@@ -0,0 +1,23 @@
+const handleProfile=(req,res,db)=>{
+  const { id }=req.params;
+  db.select('*').from('users').where({id})
+    .then(user=>{
+      if(user.length)
+      {
+        res.json(user[0])
+      }else
+      {
+        res.status(400).json('Not Found')
+      }
+    
+  })
+    .catch(err=>res.status(400).json('Error getting user'))
+  // if(!found)
+  //   {
+  //     res.json('No Such User......');
+  //   }
+}
+
+module.exports={
+  handleProfile: handleProfile
+}
