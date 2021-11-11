e8f082e11f4983f62f44f1fe8a81a8dea8aa1da4
Vardhitjain <vardhit.jain11@gmail.com>
2021-11-11 11:49:16 +0530

    some update

--- server.js
+++ server.js
@@ -40,6 +40,6 @@ app.put('/image',(req,res)=>{image.handleImage(req,res,db)})
 
 app.post('/imageurl',(req,res)=>{image.handleApiCall(req,res)})
 
-app.listen(process.env.PORT||44789,()=>{
+app.listen(process.env.PORT||3000,()=>{
   console.log(`Running on port ${process.env.PORT}`);
 })
